510875d368bffdf02ee4a728b1b139289cdbd7ae
eshers <espen.hersdal@norgesgruppen.no>
2022-12-08 16:11:52 +0100

    day 4 in progress

---
+++ Day4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Day4{
+    public static class Program {
+        public static void Main(string[] args){
+            string text = File.ReadAllText(@"C:\Users\eshers\OneDrive - NorgesGruppen\Skrivebord\aoc\Day4\input.txt");
+            IEnumerable<int> test = Enumerable.Range(1, 10);
+
+            
+
+            foreach(string line in text.Split("\n")){
+                int firstNumFirstPair = Int32.Parse(line.Split(",")[0].Split("-")[0]);
+                int secondNumFirstPair = Int32.Parse(line.Split(",")[0].Split("-")[1]);
+                int firstNumSecondPair = Int32.Parse(line.Split(",")[1].Split("-")[0]);
+                int secondNumSecondPair = Int32.Parse(line.Split(",")[1].Split("-")[1]);
+                // Console.WriteLine(firstNumFirstPair + "-" + secondNumFirstPair + "," + firstNumSecondPair + "-" + secondNumSecondPair);
+
+                IEnumerable<int> firstPair = Enumerable.Range(firstNumFirstPair, secondNumFirstPair);
+                IEnumerable<int> secondPair = Enumerable.Range(firstNumSecondPair, secondNumSecondPair);
+
+                string result = string.Join(",",secondPair);
+
+                Console.WriteLine(result);
+
+                foreach(int num in firstPair){
+                    // Console.WriteLine(num);
+                    // Console.WriteLine("________");
+                }
+
+            }
+
+            foreach(int num in test){
+                // Console.WriteLine(num);
+            }
+        }
+    }
+}
