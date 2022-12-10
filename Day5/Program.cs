926982ba69f0baeb9dc20efdd81a153f120a8cde
Espen Hersdal <espen.hersdal@tietoevry.com>
2022-12-11 00:38:53 +0100

    day 5

---
+++ Day5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Day5{
+    public static class Day5 {
+        public static void Main(string[] args){
+            string text = File.ReadAllText(@"C:\Users\hersdesp\source\repos\AdventOfCode2022\Day5\input.txt");
+
+            // Console.WriteLine((text.Split("\r\n")[0].Length + 1) / 4);
+
+            List<List<string>> list = new List<List<string>>();
+            for(int i = 0; i < 9; i++){
+                list.Add(new List<string>());
+            }
+
+            for(int i = 0; i < 9; i++){
+                // list[i].Add();
+            }
+        }
+    }
+}
