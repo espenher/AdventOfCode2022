639f3c037471e32195b071cabb489acc98fcd065
Espen Hersdal <espen.hersdal@tietoevry.com>
2022-12-10 22:03:32 +0100

    day 4

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -4,35 +4,50 @@
 namespace Day4{
     public static class Program {
         public static void Main(string[] args){
-            string text = File.ReadAllText(@"C:\Users\eshers\OneDrive - NorgesGruppen\Skrivebord\aoc\Day4\input.txt");
+            string text = File.ReadAllText(@"C:\Users\hersdesp\source\repos\AdventOfCode2022\Day4\input.txt");
             IEnumerable<int> test = Enumerable.Range(1, 10);
 
             
-
+            int fullOverlapCounter = 0;
+            int partialOverlapCounter = 0;
             foreach(string line in text.Split("\n")){
                 int firstNumFirstPair = Int32.Parse(line.Split(",")[0].Split("-")[0]);
                 int secondNumFirstPair = Int32.Parse(line.Split(",")[0].Split("-")[1]);
                 int firstNumSecondPair = Int32.Parse(line.Split(",")[1].Split("-")[0]);
                 int secondNumSecondPair = Int32.Parse(line.Split(",")[1].Split("-")[1]);
-                // Console.WriteLine(firstNumFirstPair + "-" + secondNumFirstPair + "," + firstNumSecondPair + "-" + secondNumSecondPair);
 
-                IEnumerable<int> firstPair = Enumerable.Range(firstNumFirstPair, secondNumFirstPair);
-                IEnumerable<int> secondPair = Enumerable.Range(firstNumSecondPair, secondNumSecondPair);
-
-                string result = string.Join(",",secondPair);
-
-                Console.WriteLine(result);
-
-                foreach(int num in firstPair){
-                    // Console.WriteLine(num);
-                    // Console.WriteLine("________");
+                // part 1
+                if(firstNumFirstPair <= firstNumSecondPair && secondNumFirstPair >= secondNumSecondPair){
+                    // Console.WriteLine(
+                    //     firstNumFirstPair + "-" + 
+                    //     secondNumFirstPair + "," + 
+                    //     secondNumFirstPair + "-" + 
+                    //     secondNumSecondPair
+                    // );
+                    fullOverlapCounter++;
+                } else if (firstNumFirstPair >= firstNumSecondPair && secondNumFirstPair <= secondNumSecondPair){
+                    // Console.WriteLine(
+                    //     firstNumFirstPair + "-" + 
+                    //     secondNumFirstPair + "," + 
+                    //     secondNumFirstPair + "-" + 
+                    //     secondNumSecondPair
+                    //     );
+                    fullOverlapCounter++;
                 }
-
+                // part 2 
+                if((firstNumFirstPair >= firstNumSecondPair && firstNumFirstPair <= secondNumSecondPair) || 
+                    (firstNumSecondPair >= firstNumFirstPair && firstNumSecondPair <= secondNumFirstPair)){
+                    Console.WriteLine(
+                        firstNumFirstPair + "-" + 
+                        secondNumFirstPair + "," + 
+                        firstNumSecondPair + "-" + 
+                        secondNumSecondPair
+                    );
+                    partialOverlapCounter++;
+                }
             }
 
-            foreach(int num in test){
-                // Console.WriteLine(num);
-            }
+            Console.WriteLine(partialOverlapCounter);
         }
     }
 }
